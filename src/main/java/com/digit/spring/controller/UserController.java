ce75a2717259bfe1e7bdec97a087f390c1a63200
Reeshu.Raj <Reeshu.Raj@LDIGITBGL-L0135.godigit.corp>
2023-08-21 19:33:15 +0530

    first controller with db

--- src/main/java/com/digit/spring/controller/UserController.java
+++ src/main/java/com/digit/spring/controller/UserController.java
@@ -1,11 +1,25 @@
 package com.digit.spring.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import com.digit.spring.entity.User;
+import com.digit.spring.service.UserService;
 
 @RestController
 @RequestMapping("/api/user")
 public class UserController {
 	
+	@Autowired
+	private UserService userService;
+	
+	@GetMapping("/{uid}")
+	public User getOneUser(@PathVariable Long uid) {
+		User oneUser = userService.getOneUser(uid);
+		return oneUser;
+//		return "success";
+	}
 }
--- src/main/java/com/digit/spring/service/UserService.java
+++ src/main/java/com/digit/spring/service/UserService.java
@@ -1,5 +1,18 @@
 package com.digit.spring.service;
 
-public class UserService {
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
 
+import com.digit.spring.entity.User;
+import com.digit.spring.repository.UserRepository;
+
+@Service
+public class UserService {
+	
+	@Autowired
+	private UserRepository userRepo;
+
+	public User getOneUser(Long uid) {
+		 return userRepo.getReferenceById(uid);
+	}
 }
