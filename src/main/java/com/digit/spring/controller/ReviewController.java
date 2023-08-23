74e576bc7db7cf571b29feb68a661907b778d3f1
Reeshu.Raj <Reeshu.Raj@LDIGITBGL-L0135.godigit.corp>
2023-08-23 10:55:00 +0530

    sdf

--- src/main/java/com/digit/spring/controller/ReviewController.java
+++ src/main/java/com/digit/spring/controller/ReviewController.java
@@ -4,6 +4,8 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -22,6 +24,12 @@ public class ReviewController {
 	public ResponseEntity<ReviewDTO> addReview(@RequestBody ReviewDTO reviewDto){
 		ReviewDTO review = reviewService.addReview(reviewDto);
 		return new ResponseEntity<>(review,HttpStatus.CREATED);
+	}
+	
+	@GetMapping("/{rid}")
+	public ResponseEntity<String> getReview(@PathVariable Long rid){
+//		ReviewDTO review = reviewService.addReview(reviewDto);
+		return new ResponseEntity<>("review success",HttpStatus.CREATED);
 	}
 
 }
